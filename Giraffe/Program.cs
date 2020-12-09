37f9d2bbb8a3abc5fdb8f266297f139066563b16
jaanviprabhakar <jaanviprabhakar@gmail.com>
2020-12-10 05:25:33 +0530

    Strings

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -6,14 +6,21 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            string phrase = "Giraffe Academy";
-            char grade = 'A' ;
-            int age = -30;
-            double gpa = 9.62;
-            decimal salary = 200000.252525252525;
-            bool isFemale = true;
-            Console.WriteLine("Hello");
-            Console.WriteLine(30);
+            string phrase = "Panda Academy" + " is cool.";
+           
+            Console.WriteLine("New Line: Giraffe\nAcademy");
+            Console.WriteLine("Printing Quuotes: Giraffe\"Academy");
+            Console.WriteLine(phrase);
+            Console.WriteLine("\nTrying Methods in C#: ");
+            Console.WriteLine("The length is: " + phrase.Length );
+            Console.WriteLine("In Upper Case: " + phrase.ToUpper());
+            Console.WriteLine("In Lower Case: " + phrase.ToLower());
+            Console.WriteLine("Does the string contain Academy? " + phrase.Contains("Academy"));
+            Console.WriteLine("3rd character: " + phrase[2]);
+            Console.WriteLine("To check the index of Acad: " + phrase.IndexOf("Acad"));
+            Console.WriteLine("To check the index of a: " + phrase.IndexOf('a'));
+            Console.WriteLine("To check the index of z: " + phrase.IndexOf('z'));
+            Console.WriteLine("Substring Aca: " + phrase.Substring(6,3) );
 
             Console.ReadLine();
         }
