61643693ac4d329462992b87b72b9d2e673b5c8f
jaanviprabhakar <jaanviprabhakar@gmail.com>
2020-12-10 05:02:38 +0530

    Variables

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -6,10 +6,15 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
+            string characterName = "John";
+            int characterAge = 35;
+
+            Console.WriteLine("There was once a man named " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
+
+            characterName = "Mike";
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("But he didn't like being " + characterAge);
 
             Console.ReadLine();
         }
