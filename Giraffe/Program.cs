1e2437eb15bc8a690edcab29101fed95a0a2295d
jaanviprabhakar <jaanviprabhakar@gmail.com>
2020-12-10 04:43:42 +0530

    Initial commit

---
+++ Giraffe/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+
+            Console.ReadLine();
+        }
+    }
+}
