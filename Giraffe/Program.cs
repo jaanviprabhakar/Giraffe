52e8de85f22d02597e5baa5513fde25092066d83
jaanviprabhakar <jaanviprabhakar@gmail.com>
2020-12-21 23:05:41 +0530

    2D Arrays

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -6,25 +6,45 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(GetPow(3, 2));
-            Console.WriteLine(GetPow(4, 3));
+            int[,] numberGrid = {
+                { 1, 2 },
+                { 3, 4 },
+                { 5, 6 }
+            };
 
+            for( int i=0; i<3;i++)
+            {
+                for( int j=0; j<2; j++)
+                {
+                    Console.WriteLine("Element at row "+i+" column "+j+" : "+numberGrid[i,j]);
+                }
+                
+            }
+
+            string[,] animals = new string[2, 3];
+            Console.WriteLine("\nEnter 2 groups of 3 animal names.\n");
+            for( int i=0; i<2; i++)
+            {
+                for(int j=0; j<3; j++)
+                {
+                    Console.Write("Enter animal name: ");
+                    animals[i,j]=Console.ReadLine();
+                }
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(animals[i,j]+ " ");
+                   
+                }
+                Console.WriteLine();
+            }
             Console.ReadLine();
         }
 
-        static int GetPow(int baseNum, int powNum)
-        {
-            int result = 1;
-
-            for(int i = 0; i < powNum; i++)
-            {
-                result *= baseNum; 
-
-            }
-
-            return result;
-
-        }
     }
 }
 
